21eab6672b18fc0786162b4543ad34d1d1220167
Dubois Gabriel <dubois.gabriel@etu-u.bordeaux.fr>
2020-02-02 12:52:53 +0100

    m

--- Mario/TD02/Sources/v0/include/animation.h
+++ Mario/TD02/Sources/v0/include/animation.h
@@ -11,7 +11,7 @@
 #include "map.h"
 
 void animation_init ();
-void animation_one_step (int left, int right, int up, int down, int espace);
+void animation_one_step (int left, int right, int up, int down, int espace); 
 void animation_render_objects ();
 void animation_mobile_object_add (dynamic_object_t *object);
 void animation_mobile_object_del (dynamic_object_t *object);
